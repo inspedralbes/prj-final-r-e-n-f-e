73d9bd9f0c4b0ad5ea5a24f2916115e96aca2273
a24polmolmun <a24polmolmun@inspedralbes.cat>
2026-02-26 09:17:30 +0100

    He fet que els textos de la pàgina inicial de admin canvien  depenent on estigui de cada pàgina de admin, i he començat a fer el CRUD de períodes.

--- front/src/app/features/administracio/administracio.component.ts
+++ front/src/app/features/administracio/administracio.component.ts
@@ -1,13 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'app-administracio',
-    imports: [RouterModule],
+    imports: [CommonModule, RouterModule],
     templateUrl: './administracio.component.html',
     styleUrl: './administracio.component.css'
 })
-export class AdministracioComponent  {
+export class AdministracioComponent {
+    
+  titolPagina: string = 'Resum General'; 
+
+  constructor(private router: Router) {
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.actualitzarTitol(event.urlAfterRedirects);
+      }
+    });
+  }
 
 
+  actualitzarTitol(url: string) {
+    if (url.includes('gestio-cicles')) {
+      this.titolPagina = 'Gestió de Cicles Formatius';
+    } else if (url.includes('crear-cicle')) {
+      this.titolPagina = 'Crear un Nou Cicle';
+    } else if (url.includes('editar-cicle')) {
+      this.titolPagina = 'Edició de Cicle';
+    } else if (url.includes('gestio-usuaris')) {
+      this.titolPagina = "Gestió d'Usuaris";
+    } else if (url.includes('parametres')) {
+      this.titolPagina = 'Paràmetres del Sistema';
+    } else {
+
+      this.titolPagina = 'Resum General';
+    }
+  }
 }
