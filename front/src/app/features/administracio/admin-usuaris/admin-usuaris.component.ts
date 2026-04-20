1637689b1e91deefc8276feedf3d9c4237aad434
AlbaS-R <albasanchezromero345@gmail.com>
2026-04-20 22:42:57 +0200

    Arreglos personales de docker

--- front/src/app/features/administracio/admin-usuaris/admin-usuaris.component.ts
+++ front/src/app/features/administracio/admin-usuaris/admin-usuaris.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SidebarAdminComponent } from '../../../shared/components/sidebaradmin/sidebar.component';
 
 @Component({
     selector: 'app-admin-usuaris',
-    imports: [],
+    imports: [SidebarAdminComponent],
     templateUrl: './admin-usuaris.component.html',
     styleUrl: './admin-usuaris.component.css'
 })
--- front/src/app/features/administracio/administracio.component.ts
+++ front/src/app/features/administracio/administracio.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SidebarAdminComponent } from '../../shared/components/sidebaradmin/sidebar.component';
 
 @Component({
     selector: 'app-administracio',
-    imports: [],
+    imports: [SidebarAdminComponent],
     templateUrl: './administracio.component.html',
     styleUrl: './administracio.component.css'
 })
