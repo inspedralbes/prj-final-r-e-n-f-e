feb482065c314275084a4adf1a6ef9d787cede2d
a22tonmarmar <a22tonmarmar@inspedralbes.cat>
2026-05-13 09:43:14 +0200

    bug-fix: navbar de professor al veure un perfil d'alumne

--- front/src/app/features/perfil/perfil.component.ts
+++ front/src/app/features/perfil/perfil.component.ts
@@ -87,7 +87,7 @@ export class PerfilComponent implements OnInit {
   }
 
   get isProfeOrAdmin(): boolean {
-    const rol = this.user()?.rol;
+    const rol = this.currentUser()?.rol;
     return rol === 'Profe' || rol === 'Admin';
   }
 
