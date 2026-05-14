35fd2234c2683283c6395d90c36644afa4983657
a22tonmarmar <a22tonmarmar@inspedralbes.cat>
2026-05-14 19:33:31 +0200

    feat: imatge al sidebar

--- front/src/app/shared/components/sidebar/alumnes/sidebarAlumne.component.ts
+++ front/src/app/shared/components/sidebar/alumnes/sidebarAlumne.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SidebarService } from '../../../services/sidebar.service';
+import { PerfilService } from '../../../services/perfil/perfil.service';
 
 @Component({
   selector: 'alumne-sidebar',
@@ -10,4 +11,24 @@ import { SidebarService } from '../../../services/sidebar.service';
 })
 export class SidebarAlumneComponent {
   public sidebarService = inject(SidebarService);
+  private perfilService = inject(PerfilService);
+
+  userPhoto = signal<string | null>(null);
+
+  constructor() {
+    this.loadUserPhoto();
+  }
+
+  private async loadUserPhoto() {
+    const userStr = localStorage.getItem('user');
+    if (userStr) {
+      const user = JSON.parse(userStr);
+      if (user.id) {
+        const perfil = await this.perfilService.getPerfil(String(user.id));
+        if (perfil?.data?.user?.photo) {
+          this.userPhoto.set(perfil.data.user.photo);
+        }
+      }
+    }
+  }
 }
