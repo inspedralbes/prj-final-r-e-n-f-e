3e1ce38ec49784c7f382c71f3d34d7bd7306d361
TonyMartin <a22tonmarmar@inspedralbes.cat>
2026-05-21 03:37:35 +0200

    altre

--- back/laravel-api/app/Http/Controllers/CartaFaltesController.php
+++ back/laravel-api/app/Http/Controllers/CartaFaltesController.php
@@ -113,7 +113,7 @@ class CartaFaltesController extends Controller
             $wordBase64 = base64_encode($wordFileContent);
 
             error_log('Enviant fitxer a Node API per conversió a PDF: ' . $fileName);
-            $response = Http::timeout(120)->post($_ENV['NODE_PRINCIPAL_API_URL'] . '/api/convert/word-to-pdf', [
+            $response = Http::timeout(120)->post($_ENV['NODE_PRINCIPAL_API_URL'] . 'api/convert/word-to-pdf', [
                 'fileBase64' => $wordBase64,
                 'fileName' => $fileName,
             ]);
